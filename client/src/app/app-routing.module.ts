98605724441ef8b09923af417176e2dd1044f1ca
Khoi <khoi.ntm22707@sinhvien.hoasen.edu.vn>
2023-03-06 11:13:23 +0700

    pullbai

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) }, { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) }, { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }, { path: 'player', loadChildren: () => import('./pages/player/player.module').then(m => m.PlayerModule) }, { path: 'raking', loadChildren: () => import('./pages/raking/raking.module').then(m => m.RakingModule) }, { path: 'create-quiz', loadChildren: () => import('./pages/create-quiz/create-quiz.module').then(m => m.CreateQuizModule) }];
+const routes: Routes = [{ path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) }, { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) }, { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }, { path: 'player', loadChildren: () => import('./pages/player/player.module').then(m => m.PlayerModule) }, { path: 'raking', loadChildren: () => import('./pages/raking/raking.module').then(m => m.RakingModule) }, { path: 'create-quiz', loadChildren: () => import('./pages/create-quiz/create-quiz.module').then(m => m.CreateQuizModule) }, { path: 'library', loadChildren: () => import('./pages/library/library.module').then(m => m.LibraryModule) }, { path: 'join-game', loadChildren: () => import('./pages/join-game/join-game.module').then(m => m.JoinGameModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
