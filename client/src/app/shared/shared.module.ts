9f570894bfaddb63d562db950040b76bd4bd2097
HungWah2 <123917236+HungWah2@users.noreply.github.com>
2023-03-10 17:36:16 +0700

    update UI create vs navbar

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from "@angular/core";
 import { NarBarComponent } from "../components/nar-bar/nar-bar.component";
 import { TaskquizComponent } from "../components/taskquiz/taskquiz.component";
 import { TrendingComponent } from "../components/trending/trending.component";
+import { NavBarCreateComponent } from "../components/nav-bar-create/nav-bar-create.component";
+
 
 @NgModule({
   declarations: [
     NarBarComponent,
     TaskquizComponent,
-    TrendingComponent
+    TrendingComponent,
+    NavBarCreateComponent,
   ],
   imports: [
   ],
@@ -16,7 +19,8 @@ import { TrendingComponent } from "../components/trending/trending.component";
   exports: [
     NarBarComponent,
     TaskquizComponent,  
-    TrendingComponent
+    TrendingComponent,
+    NavBarCreateComponent,
   ]
 })
 export class SharedModule { }
