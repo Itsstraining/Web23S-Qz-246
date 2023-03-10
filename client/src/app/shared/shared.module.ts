b5160fb78725a1eda2385dd91b7bfbfad6cb5b07
dungnnh <dung.nnh5930@sinhvien.hoasen.edu.vn>
2023-03-10 17:08:47 +0700

    cap nhat trang home, marketplace, lobby

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from "@angular/core";
 import { NarBarComponent } from "../components/nar-bar/nar-bar.component";
 import { TaskquizComponent } from "../components/taskquiz/taskquiz.component";
 import { TrendingComponent } from "../components/trending/trending.component";
-
+import {MatMenuModule} from '@angular/material/menu';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [
     NarBarComponent,
@@ -10,13 +13,21 @@ import { TrendingComponent } from "../components/trending/trending.component";
     TrendingComponent
   ],
   imports: [
+    MatMenuModule,
+    MatExpansionModule,
+    MatSliderModule,
+    MatButtonModule
   ],
   providers: [
   ],
   exports: [
     NarBarComponent,
     TaskquizComponent,  
-    TrendingComponent
+    TrendingComponent,
+    MatMenuModule,
+    MatExpansionModule,
+    MatSliderModule,
+    MatButtonModule
   ]
 })
 export class SharedModule { }
