479398b22cfe40eda5fc4d57606d46478f15368e
Tran Quoc Duy <duy.tq22942@sinhvien.hoasen.edu.vn>
2023-03-10 14:08:19 +0700

    pull bai

--- client/src/app/pages/player/player.component.ts
+++ client/src/app/pages/player/player.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./player.component.scss']
 })
 export class PlayerComponent {
+   time = 60;
+
+ngOnInit() {
+    this.makeIteration();
+}
+
+    makeIteration = (): void => {
+    console.clear();
+    if (this.time > 0) {
+        console.log(this.time);
+        setTimeout(this.makeIteration, 1000); // 1 second waiting
+    }
+    console.log(this.time);
+    this.time -= 1;
+};
 
 }
